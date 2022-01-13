65f5b1fc098e77c8788e83a51ea02e69815a60ce
stefanpenzinger <stefan.penzinger13@gmail.com>
2022-01-13 11:24:35 +0100

    Finished task 3.1a

---
+++ src/task_3_1_a/DontDoItLikeThat.java
@@ -0,0 +1,36 @@
+package task_3_1_a;
+
+import java.io.*;
+
+/**
+ * A simple class to extract information of the xml file and place it in the corresponding container
+ */
+public class DontDoItLikeThat {
+    public static void main(String[] args) {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("src\\task_3_1_a\\WGS.xml"));
+            StringBuilder sb = new StringBuilder();
+            String line;
+
+            while ((line = br.readLine()) != null ) {
+                sb.append(line).append("\n");
+            }
+
+            String xml = sb.toString();
+
+            int startLatitude = xml.lastIndexOf("<latitude>");
+            int endLatitude = xml.indexOf("</latitude>");
+            float latitude = Float.parseFloat(xml.substring(startLatitude, endLatitude).replace("<latitude>", ""));
+
+            int startLongitude = xml.lastIndexOf("<longitude>");
+            int endLongitude = xml.indexOf("</longitude>");
+            float longitude = Float.parseFloat(xml.substring(startLongitude, endLongitude).replace("<longitude>", ""));
+
+            WGS wgs = new WGS(latitude, longitude);
+
+            System.out.println(wgs);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/task_3_1_a/WGS.java
@@ -0,0 +1,55 @@
+package task_3_1_a;
+
+/**
+ * The data container of WGS
+ */
+public class WGS {
+    /**
+     * @val float mLatitude
+     * The latitude of the WGS
+     */
+    private float mLatitude;
+    /**
+     * @val float mLongitude
+     * The longitude of the WGS
+     */
+    private float mLongitude;
+
+    /**
+     * The constructor of WSG
+     * @param _latitude latitude of the WSG
+     * @param _longitude longitude of the WSG
+     */
+    public WGS(float _latitude, float _longitude) {
+        this.mLatitude = _latitude;
+        this.mLongitude = _longitude;
+    }
+
+    /**
+     * Returns the latitude
+     * @return float
+     */
+    public float getLatitude() {
+        return mLatitude;
+    }
+
+    /**
+     * Returns the longitude
+     * @return float
+     */
+    public float getLongitude() {
+        return mLongitude;
+    }
+
+    /**
+     * Returns string representation of the data container
+     * @return String
+     */
+    @Override
+    public String toString() {
+        return "WGS has " +
+                "a latitude of " + mLatitude +
+                " and a longitude of " + mLongitude +
+                '.';
+    }
+}
